c3eb61d462b501d871fedaa0e467c8dcbb2c96a1
cooltrain7 <cooltrain7@hotmail.co.uk>
2019-11-24 00:46:44 +0000

    Fixed Paths to Sample Scope
    Fixed the paths from global scope to sample scope
    >config
    >model.p3d
    >rvmat

--- Weapons/Firearms/Destra3000/config.cpp
+++ Weapons/Firearms/Destra3000/config.cpp
@@ -25,12 +25,12 @@ class cfgWeapons
 		scope=2;
 		displayName="Destra 3000";
 		descriptionShort="The Destra 3000 is a semi-automatic pistol based on the Astra 3000 (Spain Circa 1948). Chambered in .45 ACP Rounds for Dayz. Model by Sebastian Ujhazi Imported by Zeroy.";
-		model="DayZ-CommunitySamples\Weapons\Firearms\Destra3000\zeroy_destra_3k.p3d";
+		model="Destra3000\zeroy_destra_3k.p3d";
 		attachments[] = {"pistolMuzzle","pistolOptics","weaponOpticsCrossbow","pistolFlashlight"};
 		magazines[]={"mag_zeroy_destra3k_9rnd"};
 		hiddenSelections[] = {"zbytek"};
-		hiddenSelectionsTextures[] = {"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\Destra_3000_co.paa"};
-		hiddenSelectionsMaterials[] = {"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000.rvmat"};
+		hiddenSelectionsTextures[] = {"Destra3000\data\Destra_3000_co.paa"};
+		hiddenSelectionsMaterials[] = {"Destra3000\data\destra_3000.rvmat"};
 		class DamageSystem
 		{
 			class GlobalHealth
@@ -38,11 +38,11 @@ class cfgWeapons
 				class Health
 				{
 					hitpoints = 100;
-					healthLevels[] = {{1.0,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000.rvmat"}},
-					{0.7,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000.rvmat"}},
-					{0.5,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_damage.rvmat"}},
-					{0.3,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_damage.rvmat"}},{0.0,
-						{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_destruct.rvmat"}}};
+					healthLevels[] = {{1.0,{"Destra3000\data\destra_3000.rvmat"}},
+					{0.7,{"Destra3000\data\destra_3000.rvmat"}},
+					{0.5,{"Destra3000\data\destra_3000_damage.rvmat"}},
+					{0.3,{"Destra3000\data\destra_3000_damage.rvmat"}},
+					{0.0,{"Destra3000\data\destra_3000_destruct.rvmat"}}};
 				};
 			};
 		};
@@ -51,8 +51,8 @@ class cfgWeapons
     {
         scope = 2;
         displayName="Destra 3000 Black";
-		hiddenSelectionsTextures[] = {"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\Destra_3000_black_co.paa"};
-		hiddenSelectionsMaterials[] = {"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_black.rvmat"};
+		hiddenSelectionsTextures[] = {"Destra3000\data\Destra_3000_black_co.paa"};
+		hiddenSelectionsMaterials[] = {"Destra3000\data\destra_3000_black.rvmat"};
 		class DamageSystem
 		{
 			class GlobalHealth
@@ -60,11 +60,11 @@ class cfgWeapons
 				class Health
 				{
 					hitpoints = 100;
-					healthLevels[] = {{1.0,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_black.rvmat"}},
-					{0.7,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_black.rvmat"}},
-					{0.5,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_damage.rvmat"}},
-					{0.3,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_damage.rvmat"}},{0.0,
-						{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_destruct.rvmat"}}};
+					healthLevels[] = {{1.0,{"Destra3000\data\destra_3000_black.rvmat"}},
+					{0.7,{"Destra3000\data\destra_3000_black.rvmat"}},
+					{0.5,{"Destra3000\data\destra_3000_damage.rvmat"}},
+					{0.3,{"Destra3000\data\destra_3000_damage.rvmat"}},
+					{0.0,{"Destra3000\data\destra_3000_destruct.rvmat"}}};
 				};
 			};
 		};
@@ -73,8 +73,8 @@ class cfgWeapons
     {
         scope = 2;
         displayName="Golden Destra 3000";
-		hiddenSelectionsTextures[] = {"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\Destra_3000_gold_co.paa"};
-		hiddenSelectionsMaterials[] = {"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_gold.rvmat"};
+		hiddenSelectionsTextures[] = {"Destra3000\data\Destra_3000_gold_co.paa"};
+		hiddenSelectionsMaterials[] = {"Destra3000\data\destra_3000_gold.rvmat"};
 		class DamageSystem
 		{
 			class GlobalHealth
@@ -82,11 +82,11 @@ class cfgWeapons
 				class Health
 				{
 					hitpoints = 100;
-					healthLevels[] = {{1.0,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_gold.rvmat"}},
-					{0.7,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_gold.rvmat"}},
-					{0.5,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_damage.rvmat"}},
-					{0.3,{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_damage.rvmat"}},{0.0,
-						{"DayZ-CommunitySamples\Weapons\Firearms\Destra3000\data\destra_3000_destruct.rvmat"}}};
+					healthLevels[] = {{1.0,{"Destra3000\data\destra_3000_gold.rvmat"}},
+					{0.7,{"Destra3000\data\destra_3000_gold.rvmat"}},
+					{0.5,{"Destra3000\data\destra_3000_damage.rvmat"}},
+					{0.3,{"Destra3000\data\destra_3000_damage.rvmat"}},
+					{0.0,{"Destra3000\data\destra_3000_destruct.rvmat"}}};
 				};
 			};
 		};
@@ -100,7 +100,7 @@ class CfgMagazines
 		scope=2;
 		displayName="9rnd Destra 3000 Magazine";
 		descriptionShort="9 rounds Magazine for the Destra 3000, loaded with .45 ACP rounds.";
-		model="DayZ-CommunitySamples\Weapons\Firearms\Destra3000\mag_zeroy_destra3k_9rnd.p3d";
+		model="Destra3000\mag_zeroy_destra3k_9rnd.p3d";
 		weight=40;
 		itemSize[]={1,2};
 		count=9;
@@ -115,6 +115,6 @@ class CfgNonAIVehicles
 	{
 		scope = 2;
 		inventorySlot = "magazine";
-		model="DayZ-CommunitySamples\Weapons\Firearms\Destra3000\mag_zeroy_destra3k_9rnd.p3d";
+		model="Destra3000\mag_zeroy_destra3k_9rnd.p3d";
 	};
 };
