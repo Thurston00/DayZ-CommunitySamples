b33d28c08085ba752a20d2baa0fba9e496db205c
DaOne <markkoky44@gmail.com>
2020-04-03 23:26:34 +1100

    Some notes.

--- Script/UISample/5_Mission/missionGameplay.c
+++ Script/UISample/5_Mission/missionGameplay.c
@@ -1,3 +1,19 @@
+/*
+    @DOC / Notes:
+
+    The keybind toggle for the menu works with MissionGameplay 
+    ( the quickest way to get your input functional, plenty of other methods such as creating 
+    your own plugin using PluginManager to manage your entire input system, this goes the same with storing 
+    the instance of your menu, MissionGameplay is not the only option )
+    The menu inherits UIScriptedMenu just as any other vanilla menu in the game.
+    
+    This is setup very similar to the way vanilla base game script creates menus. By modding MissionBase.c you are expanding on the available menus, the const UI_CUSTOM_MENU 
+    is the unique ID of your newly added menu of class UICustomMenu
+
+    This is not the only method in creating UI, however i find this method to be most simple as all EventHandlers are attached and a load of pre-written 
+    functions are there for you to use from base class UIScriptedMenu
+*/
+
 modded class MissionGameplay 
 {
     private ref UICustomMenu myCustomMenu;
